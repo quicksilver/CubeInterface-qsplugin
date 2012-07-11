8bc44b8d4fff20fe4ddbb64c1d8badd1e9f176a8
Patrick Robertson <robertson.patrick@gmail.com>
2012-07-11 18:50:50 +0100

    Remove unused files from directory

--- QSSearchProvider.m
+++
@@ -1,9 +0,0 @@
-//
-//  QSSearchProvider.m
-//  QSCubeInterfaceElement
-//
-//  Created by Nicholas Jitkoff on 7/25/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
-//
-
-#import "QSSearchProvider.h"
